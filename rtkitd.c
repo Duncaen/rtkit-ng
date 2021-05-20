48cdf17460f816b0c19157495aa42ef040fb5b24
Duncan Overbruck <mail@duncano.de>
2021-05-20 19:56:48 +0200

    rtkitd: cleanup license header

--- rtkitd.c
+++ rtkitd.c
@@ -1,16 +1,21 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
 /*
- * Copyright 2009 Lennart Poettering
- * Copyright 2010 Maarten Lankhorst
- * RealtimeKit is free software: you can redistribute it and/or modify
+ * Copyright (C) 2009 Lennart Poettering
+ * Copyright (C) 2010 Maarten Lankhorst
+ * Copyright (C) 2021 Duncan Overbruck <mail@duncano.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * RealtimeKit is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
- * along with RealtimeKit. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define _DEFAULT_SOURCE
 #define _GNU_SOURCE
